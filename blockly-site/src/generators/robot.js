9e037d7f1eb316e7c01c7bcdd5a8db228100072b
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-27 20:10:27 -0400

    Delete blockly-site/src/generators/robot.js

--- blockly-site/src/generators/robot.js
+++
@@ -1,9 +0,0 @@
-export const forBlock = Object.create(null);
-
-forBlock['moon_walk_left'] = function (block, generator) {
-  const steps = generator.valueToCode(block, 'STEPS', generator.ORDER_ATOMIC) || '0';
-  const t = generator.valueToCode(block, 'T', generator.ORDER_ATOMIC) || '0';
-
-  const code = `moonWalkLeft(${steps}, ${t});\n`;
-  return code;
-};
