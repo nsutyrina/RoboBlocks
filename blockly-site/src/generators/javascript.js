3a0475747b440f7e0ed1a1ad6fd56d5673143a19
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-27 20:12:08 -0400

    Update javascript.js
    added moonwalkleft

--- blockly-site/src/generators/javascript.js
+++ blockly-site/src/generators/javascript.js
@@ -11,12 +11,12 @@ import {Order} from 'blockly/javascript';
 // This file has no side effects!
 export const forBlock = Object.create(null);
 
+// Generator for 'add_text'
 forBlock['add_text'] = function (block, generator) {
   const text = generator.valueToCode(block, 'TEXT', Order.NONE) || "''";
   const addText = generator.provideFunction_(
     'addText',
     `function ${generator.FUNCTION_NAME_PLACEHOLDER_}(text) {
-
   // Add text to the output area.
   const outputDiv = document.getElementById('output');
   const textEl = document.createElement('p');
@@ -24,7 +24,15 @@ forBlock['add_text'] = function (block, generator) {
   outputDiv.appendChild(textEl);
 }`,
   );
-  // Generate the function call for this block.
   const code = `${addText}(${text});\n`;
   return code;
 };
+
+// Generator for 'moon_walk_left'
+forBlock['moon_walk_left'] = function (block, generator) {
+  const steps = generator.valueToCode(block, 'STEPS', Order.ATOMIC) || '0';
+  const t = generator.valueToCode(block, 'T', Order.ATOMIC) || '0';
+  const code = `moonWalkLeft(${steps}, ${t});\n`;
+  return code;
+};
+
