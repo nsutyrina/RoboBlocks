131d612b16c5b4836b891e2e138d681331cc4bc6
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-27 23:25:50 -0400

    Update robot.js

--- blockly-site/src/blocks/robot.js
+++ blockly-site/src/blocks/robot.js
@@ -25,6 +25,25 @@ Blockly.defineBlocksWithJsonArray([
     tooltip: 'Make the robot walk left',
     helpUrl: '',
   },
+  {
+    type: 'moon_walk_right',
+    message0: 'moonWalkRight steps %1 T %2',
+    args0: [
+      {
+        type: 'input_value',
+        name: 'STEPS',
+      },
+      {
+        type: 'input_value',
+        name: 'T',
+      },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 230,
+    tooltip: 'Make the robot walk right',
+    helpUrl: '',
+  },
 ]);
 
 export const blocks = [];
