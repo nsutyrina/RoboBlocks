6ca5999499df540d851dcc2c4b085bfb95c3d6d6
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-27 20:39:21 -0400

    Update index.js

--- blockly-site/src/index.js
+++ blockly-site/src/index.js
@@ -1,37 +1,36 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import * as Blockly from 'blockly';
 
-// Import blocks
 import {blocks as textBlocks} from './blocks/text';
-import {blocks as robotBlocks} from './blocks/robot';
+import {blocks as robotBlocks} from './blocks/robot'; // Your moonWalkLeft block
 
-// Import generators
 import {forBlock as textGen} from './generators/javascript';
-import {forBlock as robotGen} from './generators/robot';
 
 import {javascriptGenerator} from 'blockly/javascript';
 import {save, load} from './serialization';
 import {toolbox} from './toolbox';
 import './index.css';
 
-// Register blocks
 Blockly.common.defineBlocks(textBlocks);
 Blockly.common.defineBlocks(robotBlocks);
 
-// Register generators
 Object.assign(javascriptGenerator.forBlock, textGen);
-Object.assign(javascriptGenerator.forBlock, robotGen);
 
-// Set up Blockly workspace
 const codeDiv = document.getElementById('generatedCode').firstChild;
 const outputDiv = document.getElementById('output');
 const blocklyDiv = document.getElementById('blocklyDiv');
 const ws = Blockly.inject(blocklyDiv, {toolbox});
 
-// Run the code
 const runCode = () => {
   const code = javascriptGenerator.workspaceToCode(ws);
   codeDiv.innerText = code;
   outputDiv.innerHTML = '';
+
   try {
     eval(code);
   } catch (e) {
@@ -39,19 +38,21 @@ const runCode = () => {
   }
 };
 
-// Initial load
 load(ws);
 runCode();
 
-// Save on changes
 ws.addChangeListener((e) => {
   if (e.isUiEvent) return;
   save(ws);
 });
 
-// Re-run code on workspace change
 ws.addChangeListener((e) => {
-  if (e.isUiEvent || e.type === Blockly.Events.FINISHED_LOADING || ws.isDragging()) return;
+  if (
+    e.isUiEvent ||
+    e.type === Blockly.Events.FINISHED_LOADING ||
+    ws.isDragging()
+  ) return;
+
   runCode();
 });
 
