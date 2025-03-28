7ab9f5a55978223d7a979f2529271b738b08b0d5
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-27 21:09:39 -0400

    Update index.js

--- blockly-site/src/index.js
+++ blockly-site/src/index.js
@@ -6,26 +6,34 @@
 
 import * as Blockly from 'blockly';
 
+// Import blocks
 import {blocks as textBlocks} from './blocks/text';
-import {blocks as robotBlocks} from './blocks/robot'; // Your moonWalkLeft block
+import {blocks as robotBlocks} from './blocks/robot';
 
+// Import generators
 import {forBlock as textGen} from './generators/javascript';
+import {forBlock as robotGen} from './generators/javascript'; // using same file
 
 import {javascriptGenerator} from 'blockly/javascript';
 import {save, load} from './serialization';
 import {toolbox} from './toolbox';
 import './index.css';
 
+// Register blocks
 Blockly.common.defineBlocks(textBlocks);
 Blockly.common.defineBlocks(robotBlocks);
 
+// Register code generators
 Object.assign(javascriptGenerator.forBlock, textGen);
+Object.assign(javascriptGenerator.forBlock, robotGen);
 
+// Inject Blockly
 const codeDiv = document.getElementById('generatedCode').firstChild;
 const outputDiv = document.getElementById('output');
 const blocklyDiv = document.getElementById('blocklyDiv');
 const ws = Blockly.inject(blocklyDiv, {toolbox});
 
+// Run the generated JavaScript
 const runCode = () => {
   const code = javascriptGenerator.workspaceToCode(ws);
   codeDiv.innerText = code;
@@ -38,9 +46,11 @@ const runCode = () => {
   }
 };
 
+// Load initial state
 load(ws);
 runCode();
 
+// Save workspace state and rerun code on changes
 ws.addChangeListener((e) => {
   if (e.isUiEvent) return;
   save(ws);
@@ -52,7 +62,6 @@ ws.addChangeListener((e) => {
     e.type === Blockly.Events.FINISHED_LOADING ||
     ws.isDragging()
   ) return;
-
   runCode();
 });
 
