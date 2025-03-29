bc3abed1da0b82951a2aae6f7f6e9d06eec33852
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-03-28 20:19:50 -0400

    Update javascript.js

--- blockly-site/src/generators/javascript.js
+++ blockly-site/src/generators/javascript.js
@@ -8,7 +8,6 @@ import {Order} from 'blockly/javascript';
 
 // Export all the code generators for our custom blocks,
 // but don't register them with Blockly yet.
-// This file has no side effects!
 export const forBlock = Object.create(null);
 
 // Generator for 'add_text'
@@ -17,12 +16,11 @@ forBlock['add_text'] = function (block, generator) {
   const addText = generator.provideFunction_(
     'addText',
     `function ${generator.FUNCTION_NAME_PLACEHOLDER_}(text) {
-  // Add text to the output area.
-  const outputDiv = document.getElementById('output');
-  const textEl = document.createElement('p');
-  textEl.innerText = text;
-  outputDiv.appendChild(textEl);
-}`,
+      const outputDiv = document.getElementById('output');
+      const textEl = document.createElement('p');
+      textEl.innerText = text;
+      outputDiv.appendChild(textEl);
+    }`
   );
   const code = `${addText}(${text});\n`;
   return code;
@@ -41,5 +39,14 @@ forBlock['moon_walk_right'] = function (block, generator) {
   const steps = generator.valueToCode(block, 'STEPS', Order.ATOMIC) || '0';
   const t = generator.valueToCode(block, 'T', Order.ATOMIC) || '0';
   const code = `moonWalkRight(${steps}, ${t});\n`;
+  return code;
+};
+
+// Generator for 'walk'
+forBlock['walk'] = function (block, generator) {
+  const steps = generator.valueToCode(block, 'STEPS', Order.ATOMIC) || '0';
+  const t = generator.valueToCode(block, 'T', Order.ATOMIC) || '0';
+  const dir = generator.valueToCode(block, 'DIR', Order.ATOMIC) || '1';
+  const code = `walk(${steps}, ${t}, ${dir});\n`;
   return code;
 };
