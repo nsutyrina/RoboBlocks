6911ef0e3761b1a957a8ded257a9534dbbe43f55
danieltran247 <90417943+danieltran247@users.noreply.github.com>
2025-04-01 13:20:12 -0400

    added walk backwards and dance to robot.js

--- blockly-site/src/blocks/robot.js
+++ blockly-site/src/blocks/robot.js
@@ -10,14 +10,8 @@ Blockly.defineBlocksWithJsonArray([
     type: 'moon_walk_left',
     message0: 'moonWalkLeft steps %1 T %2',
     args0: [
-      {
-        type: 'input_value',
-        name: 'STEPS',
-      },
-      {
-        type: 'input_value',
-        name: 'T',
-      },
+      { type: 'input_value', name: 'STEPS' },
+      { type: 'input_value', name: 'T' },
     ],
     previousStatement: null,
     nextStatement: null,
@@ -29,14 +23,8 @@ Blockly.defineBlocksWithJsonArray([
     type: 'moon_walk_right',
     message0: 'moonWalkRight steps %1 T %2',
     args0: [
-      {
-        type: 'input_value',
-        name: 'STEPS',
-      },
-      {
-        type: 'input_value',
-        name: 'T',
-      },
+      { type: 'input_value', name: 'STEPS' },
+      { type: 'input_value', name: 'T' },
     ],
     previousStatement: null,
     nextStatement: null,
@@ -48,23 +36,37 @@ Blockly.defineBlocksWithJsonArray([
     type: 'walk',
     message0: 'walk steps %1 T %2 dir %3',
     args0: [
-      {
-        type: 'input_value',
-        name: 'STEPS',
-      },
-      {
-        type: 'input_value',
-        name: 'T',
-      },
-      {
-        type: 'input_value',
-        name: 'DIR',
-      },
+      { type: 'input_value', name: 'STEPS' },
+      { type: 'input_value', name: 'T' },
+      { type: 'input_value', name: 'DIR' },
     ],
     previousStatement: null,
     nextStatement: null,
     colour: 230,
     tooltip: 'Make the robot walk forward or backward',
+    helpUrl: '',
+  },
+  {
+    type: 'dance',
+    message0: 'dance',
+    args0: [],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 300,
+    tooltip: 'Make the robot dance!',
+    helpUrl: '',
+  },
+  {
+    type: 'walk_backward',
+    message0: 'walk backward steps %1 T %2',
+    args0: [
+      { type: 'input_value', name: 'STEPS' },
+      { type: 'input_value', name: 'T' },
+    ],
+    previousStatement: null,
+    nextStatement: null,
+    colour: 230,
+    tooltip: 'Make the robot walk backward',
     helpUrl: '',
   },
 ]);
